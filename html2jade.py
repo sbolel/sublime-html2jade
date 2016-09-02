2b196fd4ce4f5be6b6c4f707d2cc15a830dcbc50
luxueyan <281545294@qq.com>
2016-09-02 13:28:44 +0800

    update for sublime 3
    update some code for sublime3

--- html2jade.py
+++ html2jade.py
@@ -11,7 +11,7 @@ class HtmlToJadeFromFileCommand(sublime_plugin.TextCommand):
         if target:
             with open(source, 'r') as f:
                 html = f.read()
-            jade = HTHTools.convertHTML2Jade(html)
+            jade = HTHTools.convertHTML2Jade(self, edit, html)
             if jade != None:
                 with open(target, 'w') as f:
                     f.write(jade)
@@ -25,17 +25,17 @@ class HtmlToJadeFromSelectionCommand(sublime_plugin.TextCommand):
         for region in self.view.sel():
             if not region.empty():
                 html = self.view.substr(region)
-                jade = HTHTools.convertHTML2Jade(html)
+                jade = HTHTools.convertHTML2Jade(self, edit, html)
                 if jade != None:
                     self.view.replace(edit, region, jade)
 
     def is_enabled(self):
-        return True #return self.view.file_name().endswith(".jade")
+        return True #return self.view.file_name().eself, edit, htmndswith(".jade")
 
 class HtmlToJadeFromClipboardCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         html = sublime.get_clipboard()
-        jade = HTHTools.convertHTML2Jade(html)
+        jade = HTHTools.convertHTML2Jade(self, edit, html)
         if jade != None:
             for region in self.view.sel():
                 self.view.replace(edit, region, jade)
@@ -44,33 +44,35 @@ class HtmlToJadeFromClipboardCommand(sublime_plugin.TextCommand):
         return True #return self.view.file_name().endswith(".jade")
 
 class HTHTools:
-    @classmethod
-    def convertHTML2Jade(self, contents):
+    @staticmethod
+    def convertHTML2Jade(self, edit, contents):
         html2jade = subprocess.Popen(
             'html2jade --noemptypipe --bodyless --noattrcomma',
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            shell=True
+            shell=True,
+            executable="/bin/zsh"
         )
         output, error = html2jade.communicate(contents.encode())
 
         if error:
-            self.write_to_console(error)
-            self.window.run_command("show_panel", {"panel": "output.exec"})
+            HTHTools.write_to_console(self, edit, str(error))
+            self.view.window().run_command("show_panel", {"panel": "output.exec"})
             return None
         return output.decode()
 
-    def write_to_console(self, str):
-        self.output_view = self.window.get_output_panel("exec")
+    @staticmethod    
+    def write_to_console(self, edit, str):
+        self.output_view = self.view.window().get_output_panel("exec")
         selection_was_at_end = (
             len(self.output_view.sel()) == 1 and
             self.output_view.sel()[0] == sublime.Region(self.output_view.size())
         )
         self.output_view.set_read_only(False)
-        edit = self.output_view.begin_edit()
+        # edit = self.output_view.begin_edit()
         self.output_view.insert(edit, self.output_view.size(), str)
         if selection_was_at_end:
             self.output_view.show(self.output_view.size())
-        self.output_view.end_edit(edit)
+        # self.output_view.end_edit(edit)
         self.output_view.set_read_only(True)
